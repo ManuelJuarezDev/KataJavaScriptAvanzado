d9c008802bcfebbd1935246763b9b0467472d66b
Jesus Manuel Juarez Trejo <jesus.juarezt@coppel.com>
2022-10-04 23:07:21 -0600

    Se agregan cambios en el diseño del index

--- src/index.js
+++ src/index.js
@@ -11,63 +11,73 @@ async function ArregloPokemones( callback ) {
     });
     callback(element);
   });
-  // console.log(pokemones);
+  return pokemones;
 
 }
 
-// function consultarPokemones() {
-//   return new Promise((resolve, reject) => {
-//     getPokemones().then((resp) => {
-//       let respuesta =  resp.results.map((element) => {
-//         getPokemonDetails(element.url).then((resp) => {
-//           element.id = resp.id
-//           element.type = resp.types
-//           element.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${element.id}.png`
-//         })
-//         return element;
-//       })
-//       resolve(respuesta)
-//     })
-//   })
-// }
+async function getPokemones() {
+  return axios.get('https://pokeapi.co/api/v2/pokemon/')
+}
+
+async function getPokemonDetails(url) {
+  return axios.get(url)
+}
+
+
+function ArregloPokemones() {
+  getPokemones().then((resp) => {
+    let _arrayPromesas = resp.data.results.map((element) => getPokemonDetails(element.url))
+    Promise.all(_arrayPromesas).then((response) => cargarData(response))
+  })
+}
 
 
 async function cargarData(pokemones) {
+  console.log(pokemones.length)
+  console.log(pokemones[0].data.sprites.other['official-artwork'].front_default)
+  console.log(pokemones[0].data.id)
+  console.log(pokemones[0].data.name)
+  console.log(pokemones[0].data.types[0].type['name'])
 
-  console.log(pokemones)
-  console.log( pokemones.type.length)
-
-  // for (let index1 = 0; index1 > pokemones.length; index1++){
+  for (let index1 = 0; index1 < pokemones.length; index1++){
     var getParentElement = document.getElementById('pokemones')
 
     const createPokeCard = document.createElement('div')
     createPokeCard.classList.add('card', 'col-md-3', 'p-2')
 
     const imgPokeCard = document.createElement('img');
-    imgPokeCard.src = pokemones.img
+    imgPokeCard.src = pokemones[index1].data.sprites.other['official-artwork'].front_default
     createPokeCard.appendChild(imgPokeCard)
 
     const bodyPokeCard = document.createElement('div')
     bodyPokeCard.classList.add('card-body')
-
-    const namePokeCard = document.createElement('h5')
-    namePokeCard.classList.add('card-title')
-    namePokeCard.textContent = `#${pokemones.id} - ${pokemones.name}`;
     createPokeCard.appendChild(bodyPokeCard)
 
-    for (let index = 0; index > pokemones.type.length; index++) {
-      const typePokeCard = document.createElement('p')
-      typePokeCard.classList.add('badge', 'p-1', `#${pokemones.type[index].type['name']}`)
-      typePokeCard.textContent = pokemones.type[index].type['name'];
-      bodyPokeCard.appendChild(typePokeCard)
-    }
+    const numIdPokeCard = document.createElement('h5')
+    numIdPokeCard.classList.add('card-title')
+    numIdPokeCard.textContent = `n.° ${pokemones[index1].data.id}`;
+    bodyPokeCard.appendChild(numIdPokeCard)
 
+    const namePokeCard = document.createElement('h4')
+    namePokeCard.classList.add('card-title')
+    namePokeCard.textContent = capitalizarPrimeraLetra(pokemones[index1].data.name);
     bodyPokeCard.appendChild(namePokeCard)
 
+    for (let index2 = 0; index2 < pokemones[index1].data.types.length; index2++) {
+      let typePokeCard = document.createElement('p')
+      let type = pokemones[index1].data.types[index2].type['name']
+      typePokeCard.classList.add('badge', 'm-1', type)
+      typePokeCard.textContent = pokemones[index1].data.types[index2].type['name'];
+      bodyPokeCard.appendChild(typePokeCard)
+    }
     getParentElement.appendChild(createPokeCard)
-  // }
+  }
+
+  function capitalizarPrimeraLetra(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
 
 }
 
-// consultarPokemones().then( resp => console.log(resp))
-ArregloPokemones( cargarData );
+// consultarPokemones().then( resp => cargarData(resp))
+ArregloPokemones( );
